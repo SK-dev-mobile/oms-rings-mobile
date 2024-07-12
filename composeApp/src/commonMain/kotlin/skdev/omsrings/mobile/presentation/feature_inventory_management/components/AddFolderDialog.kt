09d1264d4c261080b8b6f6b70372f7a3f27bf23e
Denis (TheFoxDK) Krishtopa <krishtopadenis@gmail.com>
2024-07-12 13:15:30 +0300

    feat: Add AddFolderDialog component to InventoryManagementScreen

---
+++ composeApp/src/commonMain/kotlin/skdev/omsrings/mobile/presentation/feature_inventory_management/components/AddFolderDialog.kt
@@ -0,0 +1,50 @@
+package skdev.omsrings.mobile.presentation.feature_inventory_management.components
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import org.jetbrains.compose.resources.StringResource
+import org.jetbrains.compose.resources.stringResource
+import skdev.omsrings.mobile.presentation.feature_inventory_management.InventoryManagementScreenContract.Event
+import skdev.omsrings.mobile.presentation.feature_inventory_management.InventoryManagementScreenModel
+import skdev.omsrings.mobile.ui.components.fields.SupportingText
+import skdev.omsrings.mobile.ui.components.fields.TextField
+import skdev.omsrings.mobile.utils.fields.FormField
+import skdev.omsrings.mobile.utils.fields.collectAsMutableState
+
+@Composable
+fun AddFolderDialog(
+    newFolderField: FormField<String, StringResource>,
+    screenModel: InventoryManagementScreenModel
+) {
+    val (newFolderValue, newFolderSetter) = newFolderField.data.collectAsMutableState()
+    val newFolderError: StringResource? by newFolderField.error.collectAsState()
+
+    AlertDialog(
+        onDismissRequest = { screenModel.onEvent(Event.HideAddFolderDialog) },
+        title = { Text("Add Folder") },
+        text = {
+            TextField(
+                value = newFolderValue,
+                onValueChange = newFolderSetter,
+                isError = newFolderError != null,
+                supportingText = SupportingText(newFolderError?.let { stringResource(it) }),
+                label = { Text("Folder Name") }
+            )
+        },
+        confirmButton = {
+            Button(onClick = { screenModel.onEvent(Event.AddFolder) }) {
+                Text("Add")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = { screenModel.onEvent(Event.HideAddFolderDialog) }) {
+                Text("Cancel")
+            }
+        }
+    )
+}
