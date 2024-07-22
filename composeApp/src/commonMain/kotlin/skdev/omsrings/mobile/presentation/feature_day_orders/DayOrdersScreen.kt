dfd07d3c9a6247b25de8d771f3e7d5599c71ddba
stakancheck <stakancheck@gmail.com>
2024-07-23 02:07:33 +0300

    refactor(day orders): Add FAB to order creation screen

--- composeApp/src/commonMain/kotlin/skdev/omsrings/mobile/presentation/feature_day_orders/DayOrdersScreen.kt
+++ composeApp/src/commonMain/kotlin/skdev/omsrings/mobile/presentation/feature_day_orders/DayOrdersScreen.kt
@@ -1,5 +1,9 @@
 package skdev.omsrings.mobile.presentation.feature_day_orders
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
@@ -8,32 +12,57 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.Help
+import androidx.compose.material.icons.rounded.AddCircle
+import androidx.compose.material.icons.rounded.AddCircleOutline
+import androidx.compose.material.icons.rounded.Edit
 import androidx.compose.material.icons.rounded.Lock
 import androidx.compose.material.icons.rounded.ShoppingCart
+import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
+import dev.gitlive.firebase.firestore.Timestamp
+import dev.gitlive.firebase.firestore.fromMilliseconds
 import kotlinx.datetime.LocalDate
+import omsringsmobile.composeapp.generated.resources.Res
+import omsringsmobile.composeapp.generated.resources.create
+import omsringsmobile.composeapp.generated.resources.edit_content
+import org.jetbrains.compose.resources.stringResource
+import skdev.omsrings.mobile.domain.usecase.feature_order.CreateOrderUseCase
 import skdev.omsrings.mobile.presentation.base.BaseScreen
 import skdev.omsrings.mobile.presentation.feature_day_orders.components.OrderView
 import skdev.omsrings.mobile.presentation.feature_day_orders.enitity.OrderInfoModel
 import skdev.omsrings.mobile.presentation.feature_day_orders.enitity.generateSampleOrderInfoList
 import skdev.omsrings.mobile.presentation.feature_faq.FAQScreen
+import skdev.omsrings.mobile.presentation.feature_order_form.OrderFormScreen
 import skdev.omsrings.mobile.ui.components.helpers.RingsTopAppBar
+import skdev.omsrings.mobile.ui.components.helpers.Spacer
+import skdev.omsrings.mobile.ui.theme.values.AnimationSpec
 import skdev.omsrings.mobile.ui.theme.values.Dimens
 import skdev.omsrings.mobile.utils.datetime.DateTimePattern
 import skdev.omsrings.mobile.utils.datetime.format
+import skdev.omsrings.mobile.utils.datetime.toInstant
 
 typealias OnAction = (DayOrdersScreenContract.Event) -> Unit
 
@@ -49,6 +78,13 @@ class DayOrdersScreen(
             selectedDay.format(DateTimePattern.SIMPLE_DATE)
         }
 
+        val lazyState = rememberLazyListState()
+        val showFloatingButton by remember(lazyState) {
+            derivedStateOf {
+                lazyState.firstVisibleItemIndex == 0 && lazyState.firstVisibleItemScrollOffset < 40
+            }
+        }
+
         Scaffold(
             topBar = {
                 RingsTopAppBar(
@@ -57,6 +93,28 @@ class DayOrdersScreen(
                         navigator.pop()
                     },
                     actions = {
+                        AnimatedVisibility(
+                            visible = !showFloatingButton,
+                            enter = scaleIn(AnimationSpec.springHigh),
+                            exit = fadeOut(AnimationSpec.tweenFast),
+                        ) {
+                            IconButton(
+                                onClick = {
+                                    navigator.push(
+                                        OrderFormScreen(
+                                            selectedDate = Timestamp.fromMilliseconds(
+                                                selectedDay.toInstant().toEpochMilliseconds().toDouble()
+                                            )
+                                        )
+                                    )
+                                }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Rounded.AddCircleOutline,
+                                    contentDescription = stringResource(Res.string.create)
+                                )
+                            }
+                        }
                         IconButton(
                             onClick = {
                                 // TODO: Add action
@@ -90,6 +148,40 @@ class DayOrdersScreen(
                     }
                 )
             },
+            floatingActionButton = {
+                AnimatedVisibility(
+                    modifier = Modifier.windowInsetsPadding(WindowInsets.safeDrawing),
+                    visible = showFloatingButton,
+                    enter = scaleIn(AnimationSpec.springHigh),
+                    exit = fadeOut(AnimationSpec.tweenFast),
+                ) {
+                    ExtendedFloatingActionButton(
+                        containerColor = MaterialTheme.colorScheme.surfaceContainer,
+                        contentColor = MaterialTheme.colorScheme.primary,
+                        onClick = {
+                            navigator.push(
+                                OrderFormScreen(
+                                    selectedDate = Timestamp.fromMilliseconds(
+                                        selectedDay.toInstant().toEpochMilliseconds().toDouble()
+                                    )
+                                )
+                            )
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Rounded.AddCircleOutline,
+                            contentDescription = stringResource(Res.string.create)
+                        )
+
+                        Spacer(Dimens.spaceMedium)
+
+                        Text(
+                            text = stringResource(Res.string.create),
+                            style = MaterialTheme.typography.titleMedium
+                        )
+                    }
+                }
+            },
             contentWindowInsets = WindowInsets.statusBars
         ) { paddingValues ->
             DayOrdersScreenContent(
@@ -99,6 +191,7 @@ class DayOrdersScreen(
                 orders = remember {
                     generateSampleOrderInfoList()
                 },
+                lazyState = lazyState,
                 onAction = {
 
                 }
@@ -110,10 +203,10 @@ class DayOrdersScreen(
 @Composable
 fun DayOrdersScreenContent(
     modifier: Modifier = Modifier,
+    lazyState: LazyListState,
     orders: List<OrderInfoModel>,
     onAction: OnAction,
 ) {
-    val lazyState = rememberLazyListState()
     LazyColumn(
         modifier = modifier,
         state = lazyState,
