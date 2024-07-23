56630f939d60fd086ad7bd7d8ebcd6f257205ca6
Denis (TheFoxDK) Krishtopa <krishtopadenis@gmail.com>
2024-07-23 21:31:23 +0300

    feat: Fix bug preventing submitting the signup form
    Close: #29

--- composeApp/src/commonMain/kotlin/skdev/omsrings/mobile/presentation/feature_order_form/OrderFormScreenModel.kt
+++ composeApp/src/commonMain/kotlin/skdev/omsrings/mobile/presentation/feature_order_form/OrderFormScreenModel.kt
@@ -110,7 +110,7 @@ class OrderFormScreenModel(
         when (event) {
             is OrderFormContract.Event.OnDeliveryMethodChanged -> updateDeliveryMethod(event.method)
             is OrderFormContract.Event.OnProductSelectionEvent -> handleProductSelectionEvent(event.event)
-            OrderFormContract.Event.OnSubmitClicked -> submitOrder()
+            is OrderFormContract.Event.OnSubmitClicked -> submitOrder()
             is OrderFormContract.Event.OnBackClicked -> navigateBack()
             is OrderFormContract.Event.LoadExistingOrder -> loadExistingOrder(event.orderId)
 
@@ -259,6 +259,7 @@ class OrderFormScreenModel(
                     updateOrderUseCase(order)
                 }
                 result.ifSuccess {
+                    launchEffect(OrderFormContract.Effect.NavigateBack)
                     showSuccessNotification(isUpdate = existingOrder != null)
                 }
             }
