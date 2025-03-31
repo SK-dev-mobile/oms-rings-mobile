9871b33a2269e855914f86da32aa3c5d7ef46a6d
sergeyiksanov <sergeyiksanov25@gmail.com>
2025-03-31 09:49:19 +0300

    upd deps

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -52,6 +52,12 @@ kotlin {
 
     sourceSets {
         commonMain.dependencies {
+            // Ktor
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.serialization.kotlinx.json)
+            implementation(libs.kotlinx.serialization.json.v160)
+
             // Notifier
             api(libs.kmpnotifier)
 
